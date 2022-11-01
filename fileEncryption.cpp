2bd0b946af48554951dabe93cfc1f21741499635
Muhammet Karul <64870233+Karuulme@users.noreply.github.com>
2022-11-01 16:48:13 +0300

    print all files in file

--- fileEncryption.cpp
+++ fileEncryption.cpp
@@ -1,44 +1,50 @@
 ﻿#include <iostream>
+#include <fstream>
 #include <windows.h>
 using namespace std;
-string fileAppendage(char fileName[]) {
-    int i = 0;
-    bool control=false;
-    string sAppendage;
-    do {
-       if (fileName[i] == '.')
-            control = true;
-        if (control)
-            sAppendage+=fileName[i];
-        i++;
-    } while (fileName[i] != '\0');
-    return sAppendage;
-}
 void ColumnSize(int columnSize) {
     if (columnSize!=0) {
         for (int i = 0; i < columnSize; i++) {
-            cout << '\t';
+            for (int k = 0; k < 7;k++) {
+                cout << "-";
+            }
+            
         }
     }
 }
 void fileSubdirectories(string firstFilePath,int columnSize = 0) {
-    firstFilePath += "/*";
     WIN32_FIND_DATAA fileInformation;
-    HANDLE firstFile = FindFirstFileExA(firstFilePath.c_str(), FindExInfoStandard, &fileInformation, FindExSearchNameMatch, NULL, 0);
+    HANDLE firstFile = FindFirstFileExA((firstFilePath+ "/*").c_str(), FindExInfoStandard, &fileInformation, FindExSearchNameMatch, NULL, 0);
     do {
-        ColumnSize(columnSize);
-        cout << "file Name : " << fileInformation.cFileName << endl;
-        if (fileAppendage(fileInformation.cFileName) == "") {
-            firstFilePath.erase(firstFilePath.length()-1,1);
-            fileSubdirectories(firstFilePath +"/" + fileInformation.cFileName, columnSize + 1);
-        }
+        if ((string)fileInformation.cFileName !="." && (string)fileInformation.cFileName != "..") {
+            ColumnSize(columnSize);
+            if (GetFileAttributesA((firstFilePath + "/" + fileInformation.cFileName).c_str()) == FILE_ATTRIBUTE_DIRECTORY) {
+                cout << fileInformation.cFileName <<" + " << endl;
+                fileSubdirectories(firstFilePath + "/" + fileInformation.cFileName, columnSize + 1);
+            }
+            else {
+                cout << fileInformation.cFileName << endl;
+            }
+        }       
     } while (FindNextFileA(firstFile, &fileInformation));
 
     FindClose(firstFile);
 }
 int main()
 {
-    fileSubdirectories("C:/Users/Karuu/Desktop/fileEncryption");
+    /*
+    fstream dosya;
+    dosya.open("C:/Users/Karuu/Desktop/test/Yeni klasör/.");
+    if (dosya.is_open()) { 
+        cout << "Dosya basari ile acildi." << endl;
+    }
+    else { // Dosya açılırken bir hata oluşursa.
+        cout << "Hata: Dosya acilamadi!\n" << endl;
+    }*/
+
+
+    fileSubdirectories("C:/Users/Karuu/Desktop/socket uygulaması");
+   // fileSubdirectories("C:/Users/Karuu/Desktop/test/Yeni klasör");
     //FindClose(lpFindFileData);
     return 0;
 }
