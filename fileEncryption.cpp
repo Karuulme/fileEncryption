680f81e992303500a98faeb3455b5ec20050bbe5
Muhammet Karul <64870233+Karuulme@users.noreply.github.com>
2022-11-01 12:11:44 +0300

    navigate inside the file

--- fileEncryption.cpp
+++ fileEncryption.cpp
@@ -1,5 +1,20 @@
 ﻿#include <iostream>
+#include <windows.h>
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+    char firstFilePath[MAX_PATH] = "C:/Users/Karuu/Desktop/fileEncryption/*";
+    WIN32_FIND_DATAA fileInformation;
+    HANDLE firstFile =FindFirstFileExA(firstFilePath,FindExInfoStandard,&fileInformation, FindExSearchNameMatch,NULL, 0);
+    cout << "error." << GetLastError()<<endl;
+    do {
+        cout << fileInformation.cFileName << endl;
+    } while (FindNextFileA(firstFile, &fileInformation));
+    
+    FindClose(firstFile);
+    //FindClose(lpFindFileData);
+
+    
+    return 0;
+
 }
